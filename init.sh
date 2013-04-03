4adf92f5b2ebe93fc7535c0ecdc4ed087f05a053
Robin Cheng <hpmv118@gmail.com>
2013-04-03 06:35:30 -0700

    Fix.

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 apt-get install ubuntu-desktop nodejs ffmpeg libavcodec-extra-53 vlc
 
+userdel vmuser
+rm -rf /home/vmuser
 useradd -m vmuser
 
 rm -rf /var/vmcloud
@@ -7,6 +9,6 @@ cp -r . /var/vmcloud
 rm /var/vmcloud/init.sh
 chown -R vmuser:vmuser /var/vmcloud
 
-/usr/lib/lightdm/lightdm-set-defaults --auto-login vmuser
-echo "session-setup-script=node /var/vmcloud/bootstrap.js" >> /etc/lightdm/lightdm.conf
+/usr/lib/lightdm/lightdm-set-defaults --autologin vmuser
+grep session-setup-script /etc/lightdm/lightdm.conf || echo "session-setup-script=node /var/vmcloud/bootstrap.js" >> /etc/lightdm/lightdm.conf
 sync

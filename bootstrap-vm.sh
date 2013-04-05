175b8abfcb7235606d3bb4243d86a55997ab801c
Robin Cheng <hpmv118@gmail.com>
2013-04-05 15:07:48 -0700

    .

--- bootstrap-vm.sh
+++ bootstrap-vm.sh
@@ -1,1 +1,2 @@
+gsettings set org.gnome.desktop.screensaver lock-enabled false
 node /var/vmcloud/bootstrap-vm.js > /var/tmp/vmcloud.out 2> /var/tmp/vmcloud.err &
--- init.sh
+++ init.sh
@@ -11,5 +11,4 @@ chown -R vmuser:vmuser /var/vmcloud
 
 /usr/lib/lightdm/lightdm-set-defaults --autologin vmuser
 grep session-setup-script /etc/lightdm/lightdm.conf || echo "session-setup-script=sudo -u vmuser sh /var/vmcloud/bootstrap-vm.sh" >> /etc/lightdm/lightdm.conf
-sudo -u vmuser gsettings set org.gnome.desktop.screensaver lock-enabled false
 sync

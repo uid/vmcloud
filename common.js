0efa27e5ca0548e9056ccef07a078ef55964babc
Robin Cheng <hpmv118@gmail.com>
2013-04-04 18:13:28 -0700

    State versioning.

--- common.js
+++ common.js
@@ -2,84 +2,85 @@ var config = require('./config.js');
 var rpc = require('./rpc.js');
 
 Object.defineProperty(global, '__stack', {
-	get: function () {
-		var orig = Error.prepareStackTrace;
-		Error.prepareStackTrace = function (_, stack) {
-			return stack;
-		};
-		var err = new Error;
-		Error.captureStackTrace(err, arguments.callee);
-		var stack = err.stack;
-		Error.prepareStackTrace = orig;
-		return stack;
-	}
+    get: function () {
+        var orig = Error.prepareStackTrace;
+        Error.prepareStackTrace = function (_, stack) {
+            return stack;
+        };
+        var err = new Error;
+        Error.captureStackTrace(err, arguments.callee);
+        var stack = err.stack;
+        Error.prepareStackTrace = orig;
+        return stack;
+    }
 });
 
 function error(e) {
-	var frame = __stack[1];
-	var line = frame.getLineNumber();
-	var file = frame.getFileName();
-	console.log("Error [" + file + ":" + line + "]: " + e);
+    var frame = __stack[1];
+    var line = frame.getLineNumber();
+    var file = frame.getFileName();
+    console.log("Error [" + file + ":" + line + "]: " + e);
 }
 
 function log(msg) {
-	var frame = __stack[1];
-	var line = frame.getLineNumber();
-	var file = frame.getFileName();
-	var logmsg = "Log [" + file + ":" + line + "]: " + msg;
-	if (config.isControl) {
-		// TODO: log to file!
-		console.log(logmsg);
-	} else {
-		console.log(logmsg);
-		rpc.connect(config.control.port, config.control.host, function (remote, conn) {
-			remote.log(config.vmid, logmsg, function () {
-				conn.destroy();
-				conn.end();
-			})
-		});
-	}
+    var frame = __stack[1];
+    var line = frame.getLineNumber();
+    var file = frame.getFileName();
+    var logmsg = "Log [" + file + ":" + line + "]: " + msg;
+    if (config.isControl) {
+        // TODO: log to file!
+        console.log(logmsg);
+    } else {
+        console.log(logmsg);
+        rpc.connect(config.control.port, config.control.host, function (remote, conn) {
+            remote.log(config.vmid, logmsg, function () {
+                conn.destroy();
+                conn.end();
+            })
+        });
+    }
 }
 
 function dlog(msg) {
-	// TODO: log to file!
-	console.log(msg);
+    // TODO: log to file!
+    console.log(msg);
 }
 
 function vlog(msg) {
-	var frame = __stack[1];
-	var line = frame.getLineNumber();
-	var file = frame.getFileName();
-	console.log("Log [" + file + ":" + line + "]: " + msg);
+    var frame = __stack[1];
+    var line = frame.getLineNumber();
+    var file = frame.getFileName();
+    console.log("Log [" + file + ":" + line + "]: " + msg);
 }
 
 
 var VMStates = {
-	BUSY: 1,
-	FREE: 2,
-	READY: 3,
-	ERROR: -1
+    BUSY: 1,
+    FREE: 2,
+    READY: 3,
+    ERROR: -1
 };
 
 
 var BeliefState = {
-	BOOTING : 1,
-	WAIT : 2,
-	FREE : 3,
-	READY : 4,
-	OCCUPIED: 5,
-	ERROR: 6,
-    name: function(val) {
-        return ['', 'BOOTING', 'WAIT', 'FREE', 'READY', 'OCCUPIED', 'ERROR'][val];
+    CREATING: 0,
+    BOOTING: 1,
+    WAIT: 2,
+    FREE: 3,
+    READY: 4,
+    OCCUPIED: 5,
+    ERROR: 6,
+    name: function (val) {
+        return ['CREATING', 'BOOTING', 'WAIT', 'FREE', 'READY', 'OCCUPIED', 'ERROR'][val];
     }
 };
 
 
 exports = module.exports = {
-	error: error,
-	log: log,
-	vlog: vlog,
-	dlog: dlog,
-	VMStates : VMStates,
-	BeliefState : BeliefState
+    error: error,
+    log: log,
+    vlog: vlog,
+    dlog: dlog,
+    VMStates: VMStates,
+    BeliefState: BeliefState
 };
--- control-server.js
+++ control-server.js
@@ -17,210 +17,248 @@ var VMStates = common.VMStates;
 
 var openstackController = null;
 
+function verState(vmid, state) {
+    return {
+        state: state,
+        get: function () {
+            return this.state
+        },
+        version: 0,
+        getVer: function () {
+            return this.version;
+        },
+        set: function (newState) {
+            var oldState = this.state;
+            this.state = newState;
+            this.version++;
+            vlog("VM #" + vmid + " belief state transition: " + BeliefState.name(oldState) + " -> " + BeliefState.name(newState));
+            return this.version;
+        },
+        verSet: function (ver, newState) {
+            if (this.version == ver) {
+                this.set(newState);
+            }
+        }
+    };
+}
+
 // TODO: state versioning, so that an old async task cannot come back way later to reset a WAIT state or ERROR state.
 
 function bootNewVM() {
-	var vmid = nextVMID;
-	nextVMID++;
-	vmData[vmid] = {state: BeliefState.WAIT}; // TODO: possible issues?
-	openstackController.boot(vmid, function (server) {
-		vmData[vmid].server = server;
-		vmData[vmid].state = BeliefState.BOOTING;
-		log("Successfully booted: " + vmid + ", OpenStack instance id = " + server.id);
-	});
+    var vmid = nextVMID;
+    nextVMID++;
+    vmData[vmid] = {state: verState(vmid, BeliefState.CREATING)}; // TODO: possible issues?
+    openstackController.boot(vmid, function (server) {
+        vmData[vmid].server = server;
+        vmData[vmid].state.verSet(0, BeliefState.BOOTING);
+        log("Successfully booted: " + vmid + ", OpenStack instance id = " + server.id);
+    });
 }
 
 function killVM(vmid) {
-	var id = vmData[vmid].server.id;
-	if (!id) {
-		log("Error when killing VM: VM " + vmid + " not ready!");
-		return;
-	}
-	vmData[vmid].state = BeliefState.WAIT;
-	openstackController.kill(id, function (id) {
-		log("Successfully terminated: " + vmid + ": " + id);
-		delete vmData[vmid];
-	})
+    var vm = vmData[vmid];
+    var id = vm.server.id;
+    if (!id) {
+        log("Error when killing VM: VM " + vmid + " not ready!");
+        return;
+    }
+    vm.state.set(BeliefState.WAIT);
+    openstackController.kill(id, function (id) {
+        log("Successfully terminated: " + vmid + ": " + id);
+        delete vmData[vmid];
+    })
 }
 
 function getVMMaintainer() {
-	var numVMs = 0;
-	var maintainer = {
-		run: function () {
-			setInterval(function maintain() {
-				var currentCount = Object.keys(vmData).length;
-				if (currentCount < numVMs) {
-					// BOOT!
-					for (var i = currentCount; i < numVMs; i++) {
-						bootNewVM();
-					}
-				} else if (currentCount > numVMs) {
-					// KILL!
-					var tokill = currentCount - numVMs;
-					for (var vmid in vmData) {
-						var state = vmData[vmid].state;
-						if (state == BeliefState.BOOTING || state == BeliefState.FREE) {
-							killVM(vmid);
-							tokill--;
-						}
-						if (state == BeliefState.WAIT) {
-							tokill--; // could be in a dead state - assume dead.
-						}
-						if (tokill == 0) break;
-					}
-				}
-			}, 5000);
-		},
-		setNumVMs: function (num) {
-			numVMs = parseInt(num);
-		},
-		getNumVMs: function () {
-			return numVMs;
-		}
-	};
-	return maintainer;
+    var numVMs = 0;
+    var maintainer = {
+        run: function () {
+            setInterval(function maintain() {
+                var currentCount = Object.keys(vmData).length;
+                if (currentCount < numVMs) {
+                    // BOOT!
+                    for (var i = currentCount; i < numVMs; i++) {
+                        bootNewVM();
+                    }
+                } else if (currentCount > numVMs) {
+                    // KILL!
+                    var tokill = currentCount - numVMs;
+                    for (var vmid in vmData) {
+                        var state = vmData[vmid].state;
+                        if (state == BeliefState.BOOTING || state == BeliefState.FREE) {
+                            killVM(vmid);
+                            tokill--;
+                        }
+                        if (state == BeliefState.WAIT) {
+                            tokill--; // could be in a dead state - assume dead.
+                        }
+                        if (tokill == 0) break;
+                    }
+                }
+            }, 5000);
+        },
+        setNumVMs: function (num) {
+            numVMs = parseInt(num);
+        },
+        getNumVMs: function () {
+            return numVMs;
+        }
+    };
+    return maintainer;
 }
 
 function getVMHeartbeater() {
-	var heartbeater = {
-		run: function () {
-			setInterval(function heartbeat() {
-				for (var vmid in vmData) {
-					if (vmData[vmid].state != BeliefState.BOOTING && vmData[vmid].state != BeliefState.WAIT) {
-						vmrpc(vmid, function (remote, cb) {
-							remote.ping(cb);
-						}, function (err, result) {
-							if (err) {
-								log('Pinging VM ' + vmid + ' failed: ' + err);
-								vmData[vmid].state = BeliefState.ERROR;
-							} else if (vmData[vmid].state != BeliefState.ERROR) {
-								var mapping = {};
-								mapping[VMStates.BUSY] = BeliefState.WAIT;
-								mapping[VMStates.FREE] = BeliefState.FREE;
-								mapping[VMStates.READY] = BeliefState.READY; // TODO: Ready vs Occupied!
-								mapping[VMStates.ERROR] = BeliefState.ERROR;
-								vmData[vmid].state = mapping[result];
-							}
-						});
-					}
-				}
-			}, 5000);
-		}
-	};
-	return heartbeater;
+    return {
+        run: function () {
+            setInterval(function heartbeat() {
+                for (var vmid in vmData) {
+                    var vm = vmData[vmid];
+                    var state = vm.state.get();
+                    if (state != BeliefState.CREATING && state != BeliefState.BOOTING && state != BeliefState.WAIT) {
+                        (function (vmid, vm) {
+                            vmrpc(vmid, function (remote, cb) {
+                                remote.ping(cb);
+                            }, function (err, result) {
+                                var state = vm.state.get();
+                                if (err) {
+                                    log('Pinging VM ' + vmid + ' failed: ' + err);
+                                    vm.state.set(BeliefState.ERROR);
+                                } else if (state == BeliefState.WAIT) {
+                                    // do nothing
+                                } else if (state == BeliefState.FREE) {
+                                    if (result != VMStates.FREE) {
+                                        log('VM ' + vmid + " is not FREE but belief state is FREE!");
+                                        vm.state.set(BeliefState.ERROR);
+                                    } else {
+                                        vm.state.set(BeliefState.FREE); // refresh it
+                                    }
+                                } else if (state == BeliefState.READY || state == BeliefState.OCCUPIED) {
+                                    if (result != VMStates.READY) {
+                                        log('VM ' + vmid + ' is not READY but belief state is ' + BeliefState.name(state) + "!");
+                                    } else {
+                                        vm.state.set(state);
+                                    }
+                                }
+                            });
+                        })(vmid, vm);
+                    }
+                }
+            }, 5000);
+        }
+    };
 }
 
 function runControlServer() {
-	var rpc_server = new rpc({
-		checkin: function (vmid, callback) {
-			vlog("Checkin received from: " + vmid);
-			vmData[vmid].state = BeliefState.WAIT;
-			callback();
-		},
-		browser_event: function (vmid, data, callback) {
-			vlog("Browser event received: " + JSON.stringify(data));
-			callback();
-		},
-		log: function (vmid, msg, callback) {
-			dlog("Remote log [" + vmid + "]: " + msg);
-			callback();
-		},
-		getConfig: function(vmid, callback) {
-			vlog("VM #" + vmid + " fetching config.");
-			fs.readFile('config.js', 'utf8', function(err, data) {
-				if (err) {
-					log("Cannot fetch config file.");
-					callback('');
-				} else {
-					callback(data);
-				}
-			});
-		}
-	});
+    var rpc_server = new rpc({
+        checkin: function (vmid, callback) {
+            vlog("Checkin received from: " + vmid);
+            vmData[vmid].state.set(BeliefState.FREE);
+            callback();
+        },
+        browser_event: function (vmid, data, callback) {
+            vlog("Browser event received: " + JSON.stringify(data));
+            callback();
+        },
+        log: function (vmid, msg, callback) {
+            dlog("Remote log [" + vmid + "]: " + msg);
+            callback();
+        },
+        getConfig: function (vmid, callback) {
+            vlog("VM #" + vmid + " fetching config.");
+            fs.readFile('config.js', 'utf8', function (err, data) {
+                if (err) {
+                    log("Cannot fetch config file.");
+                    callback('');
+                } else {
+                    callback(data);
+                }
+            });
+        }
+    });
 
-	var maintainer = getVMMaintainer();
-	var heartbeater = getVMHeartbeater();
+    var maintainer = getVMMaintainer();
+    var heartbeater = getVMHeartbeater();
 
-	var app = express();
-	app.get('/set-num-vm/:num', function (req, res) {
-		log('Received web request to set number of vm to ' + req.params.num);
-		if (req.params.num > 10) {
-			log("Can't set num vms higher than 10! (temporary security measure.)");
-		} else {
-			maintainer.setNumVMs(req.params.num);
-		}
-		res.send("");
-	});
+    var app = express();
+    app.get('/set-num-vm/:num', function (req, res) {
+        log('Received web request to set number of vm to ' + req.params.num);
+        if (req.params.num > 10) {
+            log("Can't set num vms higher than 10! (temporary security measure.)");
+        } else {
+            maintainer.setNumVMs(req.params.num);
+        }
+        res.send("");
+    });
 
-    app.get('/prepare/:vmid/:url', function(req, res) {
+    app.get('/prepare/:vmid/:url', function (req, res) {
         log('Received web request to set up a firefox session pointing to home page '
             + req.params.url + ' on VM #' + req.params.vmid);
         var vmid = parseInt(req.params.vmid);
         if (vmid in vmData) {
-            var state = vmData[vmid].state; // TODO: state versioning
-            if (state == BeliefState.FREE) {
-                vmData[vmid].state = BeliefState.WAIT;
-                vmrpc(vmid, function(remote, cb) {
+            var vm = vmData[vmid]; // TODO: state versioning
+            if (vm.state.get() == BeliefState.FREE) {
+                var ver = vm.state.set(BeliefState.WAIT);
+                vmrpc(vmid, function (remote, cb) {
                     remote.prepare({
-                        profile_name : 'vmprofile',
+                        profile_name: 'vmprofile',
                         home_page: req.params.url
                     }, cb);
-                }, function(err, result) {
+                }, function (err, result) {
                     if (err) {
                         log("Error while preparing VM: " + err);
                     } else {
-                        vmData[vmid].state = result.state; // TODO: state versioning
+                        vm.state.verSet(ver, result.state == VMStates.READY ? BeliefState.READY : BeliefState.ERROR);
                     }
                 });
             } else {
-                log("VM not in the correct state! State is " + BeliefState.name(state));
+                log("VM not in the correct state! State is " + BeliefState.name(vm.state.get()));
             }
         }
         res.send('');
     });
 
-    app.get('/cleanup/:vmid', function(req, res) {
-        log('Received web request to tear down the firefox session on VM #'+req.params.vmid);
+    app.get('/cleanup/:vmid', function (req, res) {
+        log('Received web request to tear down the firefox session on VM #' + req.params.vmid);
         var vmid = parseInt(req.params.vmid);
         if (vmid in vmData) {
-            var state = vmData[vmid].state;
-            if (state == BeliefState.READY || state == BeliefState.OCCUPIED) {
-                vmData[vmid].state = BeliefState.WAIT;
-                vmrpc(vmid, function(remote, cb) {
+            var vm = vmData[vmid];
+            if (vm.state.get() == BeliefState.READY || vm.state.get() == BeliefState.OCCUPIED) {
+                var ver = vm.state.set(BeliefState.WAIT);
+                vmrpc(vmid, function (remote, cb) {
                     remote.cleanup({}, cb);
-                }, function(err, result) {
+                }, function (err, result) {
                     if (err) {
                         log("Error while cleaning up VM: " + err);
                     } else {
-                        vmData[vmid].state = result.state; // TODO: state versioning
+                        vm.state.verSet(result.state == VMStates.FREE ? BeliefState.FREE : BeliefState.ERROR);
                     }
                 });
             } else {
-                log("VM not in the correct state! State is " + BeliefState.name(state));
+                log("VM not in the correct state! State is " + BeliefState.name(vm.state.get()));
             }
         }
         res.send('');
     });
 
 
-	log("Authenticating into OpenStack and getting parameters...");
-	// prepare openstack
-	openstack.getOpenStackController(function (controller) {
-		openstackController = controller;
-		log("Openstack connected.");
+    log("Authenticating into OpenStack and getting parameters...");
+    // prepare openstack
+    openstack.getOpenStackController(function (controller) {
+        openstackController = controller;
+        log("Openstack connected.");
 
-		rpc_server.listen(config.control.port);
-		log("Server running on port " + config.control.port);
+        rpc_server.listen(config.control.port);
+        log("Server running on port " + config.control.port);
 
-		maintainer.run();
-		log("VM maintainer started.");
+        maintainer.run();
+        log("VM maintainer started.");
 
-		heartbeater.run();
-		log("Heartbeater started.");
+        heartbeater.run();
+        log("Heartbeater started.");
 
-		app.listen(config.control.external_port);
-		log("External web server started on port " + config.control.external_port);
-	});
+        app.listen(config.control.external_port);
+        log("External web server started on port " + config.control.external_port);
+    });
 }
 
 /**
@@ -230,20 +268,20 @@ function runControlServer() {
  * @param callback called with (null, result) if success, or (err) if error
  */
 function vmrpc(vmid, action, callback) {
-	var port = config.vm.port;
-	if (!('server' in vmData[vmid])) {
-		callback("VM " + vmid + " is not ready yet.");
-		return;
-	}
+    var port = config.vm.port;
+    if (!('server' in vmData[vmid])) {
+        callback("VM " + vmid + " is not ready yet.");
+        return;
+    }
 
-	var ip = vmData[vmid].server.ip;
-	rpc.connect(port, ip, function (remote, conn) {
-		action(remote, function () {
-			conn.destroy();
-			conn.end();
+    var ip = vmData[vmid].server.ip;
+    rpc.connect(port, ip, function (remote, conn) {
+        action(remote, function () {
+            conn.destroy();
+            conn.end();
             callback.apply(null, [null].concat(arguments));
-		});
-	});
+        });
+    });
 }
 
 

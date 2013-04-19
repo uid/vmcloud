b3e03d63105acdce2493655b176a05f22377b6ac
Robin Cheng <hpmv118@gmail.com>
2013-04-19 01:36:38 -0700

    .

--- bootstrap-vm.sh
+++ bootstrap-vm.sh
@@ -1,7 +1,7 @@
+git fetch --all; git reset --hard origin/master
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 cd /var/vmcloud
 mkdir -p ~/.vnc/
 cp scripts/vnc-xstartup ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
-git fetch --all; git reset --hard origin/master
 node /var/vmcloud/bootstrap-vm.js > /var/tmp/vmcloud.out 2> /var/tmp/vmcloud.err &

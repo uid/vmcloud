b92cdcd8dc568c809cc3ad7d4ac0e620fdb4de18
Robin Cheng <hpmv118@gmail.com>
2013-04-20 00:07:28 -0700

    .

---
+++ static/vmcloud.js
@@ -0,0 +1,12 @@
+var BeliefState = {
+	CREATING: 0,
+	BOOTING: 1,
+	WAIT: 2,
+	FREE: 3,
+	READY: 4,
+	OCCUPIED: 5,
+	ERROR: 6,
+	name: function (val) {
+		return ['CREATING', 'BOOTING', 'WAIT', 'FREE', 'READY', 'OCCUPIED', 'ERROR'][val];
+	}
+};

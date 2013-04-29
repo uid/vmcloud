8d7798306432c102b8d13a8bbc776bd7d2e41cb9
Robin Cheng <hpmv118@gmail.com>
2013-04-28 23:19:50 -0400

    .

--- bootstrap-control.js
+++ bootstrap-control.js
@@ -5,8 +5,7 @@ config.openstack = configParams.openstack; // a hack.. otherwise circular depend
 var openstack = require('./openstack.js');
 var rpcBuilder = require('./rpc-builder.js');
 var vmRPCInterface = require('./rpc-interfaces.js').vmInterface;
-var log = require('./common.js').log;
-log("Authenticating into Cloud service and getting parameters...");
+console.log("Authenticating into Cloud service and getting parameters...");
 openstack.getOpenStackController(function(controller) {
 	config.initControl(controller, function(ip) {
 		return rpcBuilder.rpcInterface(ip, config.vm.interface_port, vmRPCInterface);
